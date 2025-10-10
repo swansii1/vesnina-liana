72d77a8a1e680c5e5da74a353030c27bbffe1f86
Matvey <matvey.swansii@gmail.com>
2025-10-10 16:14:33 +0300

    init: start of website development for liana

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://doctor-vesnina.vercel.app",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.8,
+};
